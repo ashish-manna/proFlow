4f43d719644a00e83ec4beb0b96242bb27d99107
ashish-manna <m.ashish1920@gmail.com>
2025-08-30 14:08:08 +0530

    auth setup done with clerk

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full px-2 py-4 flex justify-center bg-blue-950">
+      <div>Made with love 💓 @Ashish</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,5 +1,28 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/Button";
+import { PenBox } from "lucide-react";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full flex justify-between px-2 py-4">
+      <h1 className="text-4xl font-bold">ProFlow</h1>
+      <div className="flex items-center justify-between gap-2">
+        <div>
+          <Button className="text-black" variant={"destructive"}>
+            Create Project <PenBox />
+          </Button>
+        </div>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button className="cursor-pointer">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
